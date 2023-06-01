eba2ffd3dc06c4d3212d19dcea2b6378508fc400
PACKOJIbHUKOB <jordn-5@yandex.ru>
2023-06-01 07:21:23 +0300

    проверте домашнюю работу 2

--- src/Main.java
+++ src/Main.java
@@ -8,27 +8,38 @@ public class Main {
         task6();
         task7();
         task8();
-
-
-
-
     }
-
     private static void task1() {
         System.out.println("задача 1");
         var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
         System.out.println("dog: " + dog);
-
+        System.out.println("cat: " + cat);
+        System.out.println("paper: " + paper);
     }
     private static void task2() {
         System.out.println("задача 2");
-        var cat = 3.6F;
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        dog+=4;
+        cat+=4;
+        paper+=4;
+        System.out.println("dog: " + dog);
         System.out.println("cat: " + cat);
-
+        System.out.println("paper: " + paper);
     }
     private static void task3() {
         System.out.println("задача 3");
+        var dog = 8.0;
+        var cat = 3.6;
         var paper = 763789;
+        dog-=3.5;
+        cat-=1.6;
+        paper-=7639;
+        System.out.println("dog: " + dog);
+        System.out.println("cat: " + cat);
         System.out.println("paper: " + paper);
     }
     private static void task4() {
@@ -39,9 +50,7 @@ public class Main {
         System.out.println("friend * 2 = " + friend);
         friend = friend / 7;
         System.out.println("friend / 7 = " + friend);
-
     }
-
     private static void task5() {
         System.out.println("задача 5");
         var frog = 3.5;
@@ -52,7 +61,6 @@ public class Main {
         System.out.println("frog / 3.5 = " + frog);
         frog = frog - 4;
         System.out.println("frog - 4: " + frog);
-
     }
     private static void task6() {
         System.out.println("задача 6");
@@ -62,9 +70,7 @@ public class Main {
         System.out.println("Общий вес боксеров = " + totalWeightBoxers + " кг! ");
         var differenceWeightBoxers = weightBoxer1 - weightBoxer2;
         System.out.println("Разница в весе боксеров = " + differenceWeightBoxers + " кг! ");
-
     }
-
     private static void task7() {
         System.out.println("задача 7");
         var weightBoxer1 = 78.2;
@@ -73,10 +79,7 @@ public class Main {
         System.out.println("Второй боксер тяжелее на = " + differenceWeightBoxers1 + " кг! ");
         var differenceWeightBoxers2 = weightBoxer2 % weightBoxer1;
         System.out.println("Разница в весе боксеров = " + differenceWeightBoxers2 + " кг! ");
-
-
     }
-
     private static void task8() {
         System.out.println("задача 8");
         var totalHoursWork = 640;
